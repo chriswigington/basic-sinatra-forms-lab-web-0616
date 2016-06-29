3b2ecfa3d5ea03e5c5b7dfdbfb943d33bc590eca
chriswigington <chriswigington@gmail.com>
2016-06-29 13:24:17 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @params = params
+    puts "used post"
+    erb :team
+  end
+
+  get '/team' do
+    erb :team
+  end
 
 end
